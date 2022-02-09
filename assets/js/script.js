1029b99740e9c812dfcbfd92552e27ab2f0233a7
aaron <afarrell002@gmail.com>
2022-02-09 14:45:48 -0700

    picking up the project to complete after months away and more knowledge of js. refactoring to complete build

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,47 +32,6 @@ const init = () => {
   getLosses();
 };
 
-function startTime() {
-  const timerCountDown = setInterval(function () {
-    secondsLeft--;
-    timerLocation.textContent = secondsLeft;
-
-    if (secondsLeft <= 5) {
-      timerLocation.style.fontSize = "4em";
-    }
-
-    if (secondsLeft <= 3) {
-      timerLocation.style.color = "red";
-    }
-
-    if (secondsLeft < 1) {
-      clearInterval(timerCountDown);
-      gameEnd();
-      return;
-    }
-  }, 1000);
-}
-
-function generateWord() {
-  wordBlanks.textContent = randomWord;
-}
-
-// function gameEnd() {
-
-// }
-
-//event listeners
-
-startButton.addEventListener("click", function () {
-  startTime();
-  generateWord();
-});
-
-resetButton.addEventListener("click", function () {
-  wins = "";
-  losses = "";
-});
-
 // pseudo text
 
 // when user clicks start, word is hidden with underscores
